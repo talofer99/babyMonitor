17e85aaa445636829377eac9a1776c03906609da
Tal OFer <talofer99@hotmail.com>
2018-06-21 22:17:37 +0300

    added arduino folder

---
+++ arduino/SoftwareSerialExample/SoftwareSerialExample.ino
@@ -0,0 +1,29 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  Serial.println("System started");
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+
+}
+
+unsigned long lastTransmition;
+
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+  //  if (lastTransmition + 2000 < millis()) {
+  //    mySerial.println("Hello, world?");
+  //  }
+}
+
