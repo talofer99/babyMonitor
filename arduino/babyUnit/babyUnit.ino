242598cad8a597993c99fc840ee694e6a627930e
Tal OFer <talofer99@hotmail.com>
2018-06-21 23:18:14 +0300

    update to baby

--- arduino/babyUnit/babyUnit.ino
+++ arduino/babyUnit/babyUnit.ino
@@ -9,31 +9,60 @@ unsigned long lastSuccessPingTime;
 unsigned long totalPingsAttempt;
 unsigned long totalSuccessPings;
 
+byte redLedPin = 3;
+byte greenLedPin = 5;
+byte blueLedPin = 6;
+
+
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  Serial.println("System started");
+  Serial.println(F("System started"));
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(9600);
 
+  // set pin output
+  pinMode(redLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+  pinMode(blueLedPin, OUTPUT);
+
+
+  testLeds();
+
+
 }
 
+
+void testLeds() {
+  digitalWrite(redLedPin, HIGH);
+  delay(150);
+  digitalWrite(redLedPin, LOW);
+
+  digitalWrite(greenLedPin, HIGH);
+  delay(150);
+  digitalWrite(greenLedPin, LOW);
+
+  digitalWrite(blueLedPin, HIGH);
+  delay(150);
+  digitalWrite(blueLedPin, LOW);
+
+}
 
 void loop() { // run over and over
   handleSerialCommunication();
 
 
   if (lastPingAttemptTime + pingEvrey < millis()) {
-    Serial.println("Sending Ping");
-    mySerial.print("P");
+    Serial.println(F("Sending Ping"));
+    mySerial.print(F("P"));
     lastPingAttemptTime = millis();
     totalPingsAttempt++;
   }
 
-  if (lastSuccessPingTime + pingEvrey < millis()) {
-    Serial.println("Lost communication");
+  if (lastSuccessPingTime + (pingEvrey * 2) < millis()) {
+    Serial.println(F("Lost communication"));
     delay(100);
   }
 
@@ -45,13 +74,19 @@ void loop() { // run over and over
 void handleSerialCommunication() {
   if (mySerial.available()) {
     char readSerial = mySerial.read();
-    Serial.print("Recived :");
+    Serial.print(F("Recived :"));
     Serial.println(readSerial);
     switch (readSerial) {
       case 'P':
-        Serial.println("Recived ping verefication");
         lastSuccessPingTime = millis();
         totalSuccessPings++;
+        Serial.println(F("Recived ping verefication"));
+        Serial.print(F("Total Pings Attempts - "));
+        Serial.println(totalPingsAttempt);
+
+        Serial.print(F("Total Success Pings - "));
+        Serial.println(totalSuccessPings);
+
         break;
       default:
         break;
