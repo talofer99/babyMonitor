da6a08dd149dfcd88f07e647e521924c5f09dd8d
Tal OFer <talofer99@hotmail.com>
2018-06-21 23:38:53 +0300

    changes to mic output

--- arduino/mic_test/mic_test.ino
+++ arduino/mic_test/mic_test.ino
@@ -1,6 +1,6 @@
 /****************************************
-Example Sound Level Sketch for the 
-Adafruit Microphone Amplifier
+  Example Sound Level Sketch for the
+  Adafruit Microphone Amplifier
 ****************************************/
 
 const int sampleWindow = 50; // Sample window width in mS (50 mS = 20Hz)
@@ -8,39 +8,44 @@ unsigned int sample;
 
 #define MIC_PIN 0
 
-void setup() 
+void setup()
 {
-   Serial.begin(9600);
-   analogReference(EXTERNAL);
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
 }
 
 
-void loop() 
+void loop()
 {
-   unsigned long startMillis= millis();  // Start of sample window
-   unsigned int peakToPeak = 0;   // peak-to-peak level
+  unsigned long startMillis = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
 
-   unsigned int signalMax = 0;
-   unsigned int signalMin = 1024;
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
 
-   // collect data for 50 mS
-   while (millis() - startMillis < sampleWindow)
-   {
-      sample = analogRead(MIC_PIN);
-      if (sample < 1024)  // toss out spurious readings
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow)
+  {
+    sample = analogRead(MIC_PIN);
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax)
       {
-         if (sample > signalMax)
-         {
-            signalMax = sample;  // save just the max levels
-         }
-         else if (sample < signalMin)
-         {
-            signalMin = sample;  // save just the min levels
-         }
+        signalMax = sample;  // save just the max levels
       }
-   }
-   peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
-   double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 3.3) / 1024;  // convert to volts
+  Serial.print(0);
+  Serial.print('\t');
 
-   Serial.println(volts);
+  Serial.print(3.3,2);
+  Serial.print('\t');
+  Serial.print(volts);
+  Serial.print("\n");
 }
